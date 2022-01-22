4ec88ce10b6ff69a3587908fbb238c6aea1af15e
PivotStick <pivoorigami@gmail.com>
2022-01-22 15:41:52 +0100

    first commit with deploy

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+const PROD = process.env.NODE_ENV === 'production';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: preprocess(),
+
+	kit: {
+		paths: {
+			base: PROD ? '/vetilaine' : ''
+		},
+		adapter: adapter(),
+		target: 'body'
+	}
+};
+
+export default config;
